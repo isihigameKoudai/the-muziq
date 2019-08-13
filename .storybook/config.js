41b0cc22d5f4029a97666ccfbd8334132051a471
koudai ishigame <koudaiishigame@koudainoMacBook-Pro.local>
2019-08-14 00:07:32 +0900

    console削除

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 
 const req = require.context("../src/client/components/", true, /\.story\.tsx$/);
-console.log(req.keys());
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
